2a1370130f1e2c4de0573c7d77e3a00eadb4fd59
sh4rdrv <dhruv.sharma@nutrien.com>
2022-05-25 15:01:31 -0600

    all sensors

--- rviz_alarms_ws/src/teleop_panel.cpp
+++ rviz_alarms_ws/src/teleop_panel.cpp
@@ -68,9 +68,11 @@ TeleopPanel::TeleopPanel( QWidget* parent)
 {
 
   QHBoxLayout* topic_layout = new QHBoxLayout;
-  topic_layout->addWidget( new QLabel( "Lidar Alarms:" ));
+  topic_layout->addWidget( new QLabel( "Left Lidars:" ));
 
   QHBoxLayout* radar_layout = new QHBoxLayout;
+
+  QHBoxLayout* right_lidar_layout = new QHBoxLayout;
 
   QVBoxLayout* layout = new QVBoxLayout;
 
@@ -131,6 +133,63 @@ TeleopPanel::TeleopPanel( QWidget* parent)
   topic_layout->addWidget(lidar_label8);
   topic_layout->addWidget(lidar_label9);
   topic_layout->addWidget(lidar_label10);
+
+
+  //Right Lidar Status
+  right_lidar_layout->addWidget( new QLabel( "Right Lidars:" ));
+  QLabel *right_lidar_label1 = new QLabel(" 1 ");
+  QLabel *right_lidar_label2 = new QLabel(" 2 ");
+  QLabel *right_lidar_label3 = new QLabel(" 3 ");
+  QLabel *right_lidar_label4 = new QLabel(" 4 ");
+  QLabel *right_lidar_label5 = new QLabel(" 5 ");
+  QLabel *right_lidar_label6 = new QLabel(" 6 ");
+  QLabel *right_lidar_label7 = new QLabel(" 7 ");
+  QLabel *right_lidar_label8 = new QLabel(" 8 ");
+  QLabel *right_lidar_label9 = new QLabel(" 9 ");
+  QLabel *right_lidar_label10 = new QLabel(" 10 ");
+
+  right_lidar_label1->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label1->setLineWidth(1);
+  right_lidar_label1->setFixedSize(20 , 20);
+  right_lidar_label2->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label2->setLineWidth(2);
+  right_lidar_label2->setFixedSize(20 , 20);
+  right_lidar_label3->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label3->setLineWidth(2);
+  right_lidar_label3->setFixedSize(20 , 20);
+  right_lidar_label4->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label4->setLineWidth(2);
+  right_lidar_label4->setFixedSize(20 , 20);
+  right_lidar_label5->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label5->setLineWidth(2);
+  right_lidar_label5->setFixedSize(20 , 20);
+  right_lidar_label6->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label6->setLineWidth(2);
+  right_lidar_label6->setFixedSize(20 , 20);
+  right_lidar_label7->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label7->setLineWidth(2);
+  right_lidar_label7->setFixedSize(20 , 20);
+  right_lidar_label8->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label8->setLineWidth(2);
+  right_lidar_label8->setFixedSize(20 , 20);
+  right_lidar_label9->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label9->setLineWidth(2);
+  right_lidar_label9->setFixedSize(20 , 20);
+  right_lidar_label10->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+  right_lidar_label10->setLineWidth(2);
+  right_lidar_label10->setFixedSize(20 , 20);
+
+
+  right_lidar_layout->addWidget(right_lidar_label1);
+  right_lidar_layout->addWidget(right_lidar_label2);
+  right_lidar_layout->addWidget(right_lidar_label3);
+  right_lidar_layout->addWidget(right_lidar_label4);
+  right_lidar_layout->addWidget(right_lidar_label5);
+  right_lidar_layout->addWidget(right_lidar_label6);
+  right_lidar_layout->addWidget(right_lidar_label7);
+  right_lidar_layout->addWidget(right_lidar_label8);
+  right_lidar_layout->addWidget(right_lidar_label9);
+  right_lidar_layout->addWidget(right_lidar_label10);
 
 
   //RADAR Status
@@ -197,6 +256,7 @@ TeleopPanel::TeleopPanel( QWidget* parent)
   bottom_layout->addWidget(hline);
   
   layout->addLayout( topic_layout );
+  layout->addLayout(right_lidar_layout);
   layout->addLayout( radar_layout );
   layout->addLayout( bottom_layout );
   setLayout( layout );
@@ -241,6 +301,9 @@ void TeleopPanel::paintEvent(QPaintEvent *event)
 
   painter.setRenderHint(QPainter::Antialiasing, true);  //Message box init
   painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
+
+
+  //Left Lidars
 
   QRect lidar_1_left_rect;
   QRect lidar_2_left_rect;
@@ -393,54 +456,311 @@ void TeleopPanel::paintEvent(QPaintEvent *event)
     painter.drawRect(lidar_10_left_rect);
   }
 
-  QPainter radar_painter(this);
+  //Right Lidars
 
-  if(alarm_status[0] == false){
-    radar_painter.setRenderHint(QPainter::Antialiasing, true);
-    radar_painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
-    radar_painter.setBrush(QBrush(Qt::green, Qt::SolidPattern));
-    radar_painter.drawRect(108, 35, 20, 20);
-    radar_painter.drawRect(135, 35, 20, 20);
-    radar_painter.drawRect(160, 35, 20, 20);
-    radar_painter.drawRect(185, 35, 20, 20);
-    radar_painter.drawRect(212, 35, 20, 20);
-    radar_painter.drawRect(238, 35, 20, 20);
-    radar_painter.drawRect(265, 35, 20, 20);
-    radar_painter.drawRect(292, 35, 20, 20);
-    radar_painter.drawRect(319, 35, 20, 20);
-    radar_painter.drawRect(346, 35, 20, 20);
+  QRect lidar_1_right_rect;
+  QRect lidar_2_right_rect;
+  QRect lidar_3_right_rect;
+  QRect lidar_4_right_rect;
+  QRect lidar_5_right_rect;
+  QRect lidar_6_right_rect;
+  QRect lidar_7_right_rect;
+  QRect lidar_8_right_rect;
+  QRect lidar_9_right_rect;
+  QRect lidar_10_right_rect;
 
+  lidar_1_right_rect.setTop(35);
+  lidar_1_right_rect.setLeft(108);
+  lidar_1_right_rect.setBottom(55);
+  lidar_1_right_rect.setRight(128);
+
+  lidar_2_right_rect.setTop(35);
+  lidar_2_right_rect.setLeft(135);
+  lidar_2_right_rect.setBottom(55);
+  lidar_2_right_rect.setRight(155);
+
+  lidar_3_right_rect.setTop(35);
+  lidar_3_right_rect.setLeft(160);
+  lidar_3_right_rect.setBottom(55);
+  lidar_3_right_rect.setRight(180);
+
+  lidar_4_right_rect.setTop(35);
+  lidar_4_right_rect.setLeft(185);
+  lidar_4_right_rect.setBottom(55);
+  lidar_4_right_rect.setRight(205);
+
+  lidar_5_right_rect.setTop(35);
+  lidar_5_right_rect.setLeft(212);
+  lidar_5_right_rect.setBottom(55);
+  lidar_5_right_rect.setRight(232); 
+
+  lidar_6_right_rect.setTop(35);
+  lidar_6_right_rect.setLeft(238);
+  lidar_6_right_rect.setBottom(55);
+  lidar_6_right_rect.setRight(258);  
+
+  lidar_7_right_rect.setTop(35);
+  lidar_7_right_rect.setLeft(265);
+  lidar_7_right_rect.setBottom(55);
+  lidar_7_right_rect.setRight(285); 
+
+  lidar_8_right_rect.setTop(35);
+  lidar_8_right_rect.setLeft(292);
+  lidar_8_right_rect.setBottom(55);
+  lidar_8_right_rect.setRight(312); 
+
+  lidar_9_right_rect.setTop(35);
+  lidar_9_right_rect.setLeft(319);
+  lidar_9_right_rect.setBottom(55);
+  lidar_9_right_rect.setRight(339); 
+
+  lidar_10_right_rect.setTop(35);
+  lidar_10_right_rect.setLeft(346);
+  lidar_10_right_rect.setBottom(55);
+  lidar_10_right_rect.setRight(366); 
+
+  if(alarm_status[0] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_1_right_rect);
   }
-  else if(alarm_status[0] == true){
-    radar_painter.setRenderHint(QPainter::Antialiasing, true);
-    radar_painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
-    radar_painter.setBrush(QBrush(Qt::yellow, Qt::SolidPattern));
-    radar_painter.drawRect(108, 35, 20, 20);
-    radar_painter.drawRect(135, 35, 20, 20);
-    radar_painter.drawRect(160, 35, 20, 20);
-    radar_painter.drawRect(185, 35, 20, 20);
-    radar_painter.drawRect(212, 35, 20, 20);
-    radar_painter.drawRect(238, 35, 20, 20);
-    radar_painter.drawRect(265, 35, 20, 20);
-    radar_painter.drawRect(292, 35, 20, 20);
-    radar_painter.drawRect(319, 35, 20, 20);
-    radar_painter.drawRect(346, 35, 20, 20);
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_1_right_rect);
+  }
 
-  } 
-  else{
-    radar_painter.setRenderHint(QPainter::Antialiasing, true);
-    radar_painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
-    radar_painter.setBrush(QBrush(Qt::gray, Qt::SolidPattern));
-    radar_painter.drawRect(108, 35, 20, 20);
-    radar_painter.drawRect(135, 35, 20, 20);
-    radar_painter.drawRect(160, 35, 20, 20);
-    radar_painter.drawRect(185, 35, 20, 20);
-    radar_painter.drawRect(212, 35, 20, 20);
-    radar_painter.drawRect(238, 35, 20, 20);
-    radar_painter.drawRect(265, 35, 20, 20);
-    radar_painter.drawRect(292, 35, 20, 20);
-    radar_painter.drawRect(319, 35, 20, 20);
-    radar_painter.drawRect(346, 35, 20, 20);
+  if(alarm_status[1] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_2_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_2_right_rect);
+  }
+
+  if(alarm_status[2] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_3_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_3_right_rect);
+  }
+
+  if(alarm_status[3] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_4_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_4_right_rect);
+  }
+
+  if(alarm_status[4] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_5_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_5_right_rect);
+  }
+
+  if(alarm_status[5] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_6_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_6_right_rect);
+  }
+
+  if(alarm_status[6] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_7_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_7_right_rect);
+  }
+
+  if(alarm_status[7] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_8_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_8_right_rect);
+  }
+
+  if(alarm_status[8] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_9_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_9_right_rect);
+  }
+
+  if(alarm_status[9] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(lidar_10_right_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(lidar_10_right_rect);
+  }
+
+
+// Radars
+
+  QRect radar_1_rect;
+  QRect radar_2_rect;
+  QRect radar_3_rect;
+  QRect radar_4_rect;
+  QRect radar_5_rect;
+  QRect radar_6_rect;
+  QRect radar_7_rect;
+  QRect radar_8_rect;
+  QRect radar_9_rect;
+  QRect radar_10_rect;
+
+  radar_1_rect.setTop(60);
+  radar_1_rect.setLeft(108);
+  radar_1_rect.setBottom(80);
+  radar_1_rect.setRight(128);
+
+  radar_2_rect.setTop(60);
+  radar_2_rect.setLeft(135);
+  radar_2_rect.setBottom(80);
+  radar_2_rect.setRight(155);
+
+  radar_3_rect.setTop(60);
+  radar_3_rect.setLeft(160);
+  radar_3_rect.setBottom(80);
+  radar_3_rect.setRight(180);
+
+  radar_4_rect.setTop(60);
+  radar_4_rect.setLeft(185);
+  radar_4_rect.setBottom(80);
+  radar_4_rect.setRight(205);
+
+  radar_5_rect.setTop(60);
+  radar_5_rect.setLeft(212);
+  radar_5_rect.setBottom(80);
+  radar_5_rect.setRight(232); 
+
+  radar_6_rect.setTop(60);
+  radar_6_rect.setLeft(238);
+  radar_6_rect.setBottom(80);
+  radar_6_rect.setRight(258);  
+
+  radar_7_rect.setTop(60);
+  radar_7_rect.setLeft(265);
+  radar_7_rect.setBottom(80);
+  radar_7_rect.setRight(285); 
+
+  radar_8_rect.setTop(60);
+  radar_8_rect.setLeft(292);
+  radar_8_rect.setBottom(80);
+  radar_8_rect.setRight(312); 
+
+  radar_9_rect.setTop(60);
+  radar_9_rect.setLeft(319);
+  radar_9_rect.setBottom(80);
+  radar_9_rect.setRight(339); 
+
+  radar_10_rect.setTop(60);
+  radar_10_rect.setLeft(346);
+  radar_10_rect.setBottom(80);
+  radar_10_rect.setRight(366); 
+
+  if(alarm_status[0] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_1_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_1_rect);
+  }
+
+  if(alarm_status[1] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_2_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_2_rect);
+  }
+
+  if(alarm_status[2] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_3_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_3_rect);
+  }
+
+  if(alarm_status[3] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_4_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_4_rect);
+  }
+
+  if(alarm_status[4] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_5_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_5_rect);
+  }
+
+  if(alarm_status[5] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_6_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_6_rect);
+  }
+
+  if(alarm_status[6] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_7_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_7_rect);
+  }
+
+  if(alarm_status[7] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_8_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_8_rect);
+  }
+
+  if(alarm_status[8] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_9_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_9_rect);
+  }
+
+  if(alarm_status[9] == true){
+    painter.setBrush(comms_ok);
+    painter.drawRect(radar_10_rect);
+  }
+  else {
+    painter.setBrush(comms_bad);
+    painter.drawRect(radar_10_rect);
   }
 
   //Message Box
